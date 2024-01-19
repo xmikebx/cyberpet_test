151a1686bce6ca9f1b91e127e29f82c66e66bea3
xmikebx <xmikebx@gmail.com>
2024-01-19 09:58:52 +0000

    tidyup

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-// const selectPet = document.getElementById(`petContainer`);
-
-// console.log(selectPet);
-
-
 const pet1 = document.getElementById(`pet1`);
 const pet2 = document.getElementById(`pet2`);
 const pet3 = document.getElementById(`pet3`);
 const petSelector = document.getElementById(`selectorCont`);
-// const listenForSelect = document.getElementById(`selector`);
+
 let currentSelection = "c";
-// const director = document.getElementById(`
+
 
 petSelector.addEventListener("click", (e) => {
     
@@ -19,18 +14,13 @@ petSelector.addEventListener("click", (e) => {
     console.log(b);
 })
 
-
 const doThing = (id) => {
-    // const currentSelection = "c";
 
     const selectedItem = document.getElementById(id)
-    console.log(selectedItem.id);
-
 
     if (selectedItem.id === "selector") {
         document.getElementById(`selectorCont`).style.display = "none";
     } else;
-
 
     switch(true) {
         case currentSelection === "c" && selectedItem.id === "selL":
@@ -43,13 +33,6 @@ const doThing = (id) => {
             pet2.style.zIndex = "-1";
             pet2.style.left = "-830px";  
 
-
-
-    
-
-            // document.getElementById(`pet3`).style.zIndex = "-1";
-            // document.getElementById(`pet3`).style.left = "-300px";
-
             document.getElementById(`selL`).style.zIndex = "-1";
 
             currentSelection = "l";   
@@ -58,14 +41,8 @@ const doThing = (id) => {
 
         case currentSelection === "c" && selectedItem.id === "selR":
 
-            // console.log(`GO TO PET 3 FROM PET 2`);
-
-            // document.getElementById(`pet1`).style.left = "300px";
-            // document.getElementById(`pet1`).style.zIndex = "-1";
-
             pet2.style.zIndex = "-1";
             pet2.style.left = "830px";
-
 
             pet3.style.zIndex = "1";
             pet3.style.left = "-830px";
@@ -78,20 +55,11 @@ const doThing = (id) => {
 
             case currentSelection === "l" && selectedItem.id === "selR":
 
-            // console.log(`GO TO PET 2 FROM PET 1`);
-
-
             pet1.style.zIndex = "-1";
             pet1.style.left = "-830px";
 
             pet2.style.zIndex = "1";
             pet2.style.left = "0px";    
-
-
-            // pet3.style.zIndex = "-1";
-            // pet3.style.left = "-300px";
-
-
 
             document.getElementById(`selL`).style.zIndex = "1";
             document.getElementById(`selR`).style.zIndex = "1";
@@ -101,11 +69,6 @@ const doThing = (id) => {
             break;
 
             case currentSelection === "r" && selectedItem.id === "selL":
-
-            // console.log(`GO TO PET 2 FROM PET 3`);
-
-            // pet1.style.zIndex = "-1";
-            // pet1.style.left = "0px";
 
             pet2.style.zIndex = "1";
             pet2.style.left = "0px";
